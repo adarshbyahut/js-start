94ac20c84059c7c99794c557582780836d9e17ed
Adarsh Kumar <156606068+adarshbyahut@users.noreply.github.com>
2024-12-28 18:38:41 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 144553
+let accountEmail="adarsh09124@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+
+accountEmail="starsh@gmail.com"
+accountPassword="0987"
+accountCity="Bihar"
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountEmail,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using nodejs,not browser
+
+/*number => 2 to power 53
+bigint
+string => ""
+boolean=> true/false
+null=> standalone value
+undefined=> 
+    symbol=>unique
+*/
+console.log(3+3)
+
+console.log("adarsh")
+
+let name="adarsh"
+let age=21
+
+console.log(typeof undefined); //value represent undefined
+console.log(typeof null); // value represent object
